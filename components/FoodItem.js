3536dc80bbfed570f9659aa5c3d5a37866a31a7c
Frederique Andolfatto <pk37fki@eduvaud.ch>
2025-02-12 18:19:44 +0100

    Form management change props name for better understanding

--- components/FoodItem.js
+++ components/FoodItem.js
@@ -1,13 +1,13 @@
 App.component('food-item', {
     props : {
-        foods : {
+        foodlist : {
             required : true,
             type : Array
         }
     },
     template:
         `<div id="wrapper"> 
-            <div v-for="(fooditem, index) in foods" :key="index">
+            <div v-for="(fooditem, index) in foodlist" :key="index">
                 <h2>{{fooditem.name}}</h2>
                 <p>{{fooditem.type}}</p>
                 <p v-if="fooditem.vegetarian">Végétarien</p>
